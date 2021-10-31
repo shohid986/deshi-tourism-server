7b9c657494d016271ba8e6349b306e618263f062
shohid986 <shohidul9863371@gmail.com>
2021-10-31 23:21:01 +0600

    server mainly added

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
+require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(cors());
-// const uri = "mongodb+srv://<username>:<password>@cluster0.oroy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// tourismMongoDB
-// Nz9TbAF4WZCoLz3e
-const uri = "mongodb+srv://tourismMongoDB:Nz9TbAF4WZCoLz3e@cluster0.oroy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oroy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
     try {
       await client.connect();
       console.log('mongo database connected successfully');
       const database = client.db("tourism-web");
-      const movies = database.collection("services");
+      const serviceCollection = database.collection("services");
+      const organizersCollection = database.collection("organizers");
+      const ordersCollection = database.collection("orders");
+
+      // services get api
       app.get('/services', async(req, res)=>{
-        const cursor = movies.find({});
-        const result = await cursor.toArray();
+        const cursor = serviceCollection.find({});
+        const services = await cursor.toArray();
+        res.json(services);
+      })
+
+      // service get api
+      app.get('/service/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const singleService = await serviceCollection.findOne(query);
+        res.json(singleService);
+      })
+
+      // organizers get api
+      app.get('/organizers', async(req, res)=>{
+        const cursor = organizersCollection.find({});
+        const organizers = await cursor.toArray();
+        res.json(organizers);
+      })
+
+      // post order api
+      app.post('/orders/:id', async(req, res)=>{
+        const order = req.body;
+        const id = req.params.id;
+        order.id= id;
+        const result = await ordersCollection.insertOne(order);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        console.log("docs hitted");
+        res.json(result);
+      })
+
+      // get orders api
+      app.get('/orders', async(req, res)=>{
+        const cursor = ordersCollection.find({});
+        const orders = await cursor.toArray();
+        res.json(orders);
+      })
+
+      //delete services api
+      app.delete('/orders/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const result = await ordersCollection.deleteOne(query);
         res.json(result);
       })
 
